d1894862b6427973e0881460f49b27dae2bc5c8a
akata72 <16230163+akata72@users.noreply.github.com>
2019-01-03 08:51:41 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -7,6 +7,6 @@ terraform {
     container_name       = "tfstateservices"
     key                  = "terraform.state.netscaler-ha-multinic"
     # need to figure out an alternative way of providing the access_key...
-    access_key           = "rcJIpwd/SmbY6WC0zgsYUv7Krhfa/0wS0B18x6mbeURnr30xQ5CCZIFxGzFZ1CA0K64aywekXSRw9L+7Yn1HoA=="
+    access_key           = ""
   }
 } 
